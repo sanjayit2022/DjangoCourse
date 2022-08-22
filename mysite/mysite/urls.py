a82405ca76fac75f55fe982a4a6eec7669ba26be
Sanjaykumar Modhawadiya <sanjaykumar.modhawadiya@oneadvanced.com>
2022-08-22 17:18:19 +0530

    Configurations of hold web app on Heroku

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path
 
 from . import views
+
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +28,8 @@ urlpatterns = [
     path('result', views.result, name='Result'),
     path('process', views.process, name='Process'),
     path('add', views.add, name='Addition'),
-    path('database', views.database, name='Database')
+    path('database', views.database, name='Database'),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
