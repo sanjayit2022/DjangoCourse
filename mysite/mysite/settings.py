7279c81beebf4dc947b13f416468487b98378034
Sanjaykumar Modhawadiya <sanjaykumar.modhawadiya@oneadvanced.com>
2022-08-22 17:45:09 +0530

    Update settings.py
    Added heroku host name in allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7w=xv6ff$l5sg*&k*mb&5z+=(dicz8o^x$5e)#8(kcjirrrp1_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','djangocourse-sm.herokuapp.com']
 
 
 # Application definition
