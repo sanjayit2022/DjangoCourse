50d6ee4967285b4375a160ef60214787c85f8754
Sanjaykumar Modhawadiya <sanjaykumar.modhawadiya@oneadvanced.com>
2022-08-23 18:35:26 +0530

    s

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # All settings common to all environments
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT =BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
