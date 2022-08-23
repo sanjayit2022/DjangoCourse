3d4f0c0a196551f9e790c74b547dd23896ddd1d5
Sanjaykumar Modhawadiya <sanjaykumar.modhawadiya@oneadvanced.com>
2022-08-23 19:10:09 +0530

    d

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7w=xv6ff$l5sg*&k*mb&5z+=(dicz8o^x$5e)#8(kcjirrrp1_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','djangocourse-sm.herokuapp.com']
 
